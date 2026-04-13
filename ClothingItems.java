01acf6e54afbe1b9f38ea36279bc557dfe1dc754
HeebaAamir <heeba.aamir@gmail.com>
2026-04-13 19:49:32 +0500

    Added class definitions for subclasses accessories and outerwear and some minor updates

--- ClothingItems.java
+++ ClothingItems.java
@@ -2,18 +2,19 @@ public class ClothingItems {
     private String size;
     private String brand;
     private int price;
-    String fabric;
-    String season;
-    String colour;
-    int WearCount;
+    private String fabric;
+    private String season;
+    private String colour;
+    private int WearCount;
 
-    public ClothingItems(String size, String brand, int price, String fabric, String colour, int WearCount){
+    public ClothingItems(String size, String brand, int price, String fabric, String colour, int WearCount, String season){
         this.size = size;
         this.brand = brand;
         this.price = price;
         this.fabric = fabric;
         this.colour = colour;
         this.WearCount = WearCount;
+        this.season = season;
     }
 
     public void setSize(String size){
@@ -34,15 +35,41 @@ public class ClothingItems {
     public int getPrice(){
         return price;
     }
-    
+    public void setColour(String colour) { 
+        this.colour = colour; 
+    }
+    public String getColour() { 
+        return colour; 
+    }
+    public void setWearCount(int WearCount) { 
+        this.WearCount = WearCount; 
+    }
+    public int getWearCount() { 
+        return WearCount; 
+    }
+    public void incrementWear() { 
+        WearCount++; 
+    }
+    public void setFabric(String fabric) { 
+        this.fabric = fabric; 
+    }
+    public String getFabric() { 
+        return fabric; 
+    }
+    public void setSeason(String season) { 
+        this.season = season; 
+    }
+    public String getSeason() { 
+        return season; 
+    } 
 }
 
 class top extends ClothingItems{
     String style;
     String sleeveType;
 
-    public top(String size, String brand, int price, String fabric, String colour, int WearCount, String style, String sleeveType){
-        super(size, brand, price, fabric, colour, WearCount);
+    public top(String size, String brand, int price, String fabric, String colour, int WearCount, String season, String style, String sleeveType){
+        super(size, brand, price, fabric, colour, WearCount, season);
         this.style = style;
         this.sleeveType = sleeveType;
     }
@@ -51,8 +78,8 @@ class top extends ClothingItems{
 class bottom extends ClothingItems{
     String style;
 
-    public bottom(String size, String brand, int price, String fabric, String colour, int WearCount, String style){
-        super(size, brand, price, fabric, colour, WearCount);
+    public bottom(String size, String brand, int price, String fabric, String colour, int WearCount, String season, String style){
+        super(size, brand, price, fabric, colour, WearCount,season);
         this.style = style;
     }
 }
@@ -62,18 +89,29 @@ class shoes extends ClothingItems{
     int heelSize;
     boolean openFront;
     
-    public shoes(String size, String brand, int price, String fabric, String colour, int WearCount, String style, int heelSize, boolean openFront){
-        super(size, brand, price, fabric, colour, WearCount);
+    public shoes(String size, String brand, int price, String fabric, String colour, int WearCount, String season, String style, int heelSize, boolean openFront){
+        super(size, brand, price, fabric, colour, WearCount,season);
         this.style = style;
         this.heelSize = heelSize;
         this.openFront = openFront;
     }
 }
 
-class outerwear extends ClothingItems{
-    string style;
-}
-
 class accessories extends ClothingItems{
-    string style;
-}
+    String type;
+
+    public accessories(String size, String brand, int price, String fabric, String colour, int WearCount, String season, String type){
+        super(size, brand, price, fabric, colour, season, WearCount);
+        this.type = type;
+    }
+}       
+
+class outerwear extends ClothingItems{
+    String style;
+
+    public outerwear(String size, String brand, int price, String fabric, String colour, int WearCount, String season, String style){
+        super(size, brand, price, fabric, colour, season, WearCount);
+        this.style = style;
+        this.style = style;
+    }
+}   
