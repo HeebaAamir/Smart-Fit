e66c697a01fdf02a36188833241ad3a0706e5fe1
HeebaAamir <heeba.aamir@gmail.com>
2026-04-19 12:00:12 +0500

    Implement Serializable interface in Outfit class
    Added Serializable interface and serialVersionUID to Outfit class.

--- Outfit.java
+++ Outfit.java
@@ -1,4 +1,7 @@
-public class Outfit {
+import java.io.Serializable
+
+public class Outfit implements Serializable {
+   private static final long serialVersionUID = 1L; 
    private String name;
    private int rating;
    private top Top;
